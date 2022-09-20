6eeb1b14f8401412627e972dec62e95411669940
Murillo AL Costa <91096652+murillocosta@users.noreply.github.com>
2022-09-19 21:22:13 -0300

    feat: card styles initialized, data formating
    Co-Authored-By: Lucas Passos <92932184+LucasBinho@users.noreply.github.com>

--- src/Components/Card/Card.jsx
+++ src/Components/Card/Card.jsx
@@ -1,14 +1,82 @@
 import styles from './Card.module.css';
 
-const Card = ({ temp, humidity, feelsLike }) => {
+import SimpleDateTime from 'react-simple-timestamp-to-date';
+
+const Card = ({
+  temp,
+  humidity,
+  feelsLike,
+  weatherDescription,
+  windDeg,
+  windSpeed,
+  sunrise,
+  sunset,
+}) => {
+  const simpleDateOptions = {
+    showDate: '0',
+    timeSeparator: ':',
+  };
+
+  const weatherCap = weatherDescription => {
+    return (
+      weatherDescription.charAt(0).toUpperCase() + weatherDescription.slice(1)
+    );
+  };
+
+  const windDirection = deg => {
+    if (deg === 0) return 'Norte';
+    if (deg > 0) return 'Nordeste';
+    if (deg === 90) return 'Leste';
+    if (deg > 90) return 'Sudeste';
+    if (deg === 180) return 'Sul';
+    if (deg > 180) return 'Sudoeste';
+    if (deg === 270) return 'Oeste';
+    if (deg > 270) return 'Noroeste';
+  };
+
   return (
-    <section>
+    <section className={styles.Card}>
       <h1>Previsão do dia:</h1>
       <aside>
         <ul>
-          <li>Temperatura: {temp}</li>
-          <li>Humidade: {humidity}</li>
-          <li>Sensação Térmica: {feelsLike}</li>
+          <li>
+            Temperatura: <span>{Math.round(temp)}° C</span>
+          </li>
+          <li>
+            Humidade: <span>{humidity}%</span>
+          </li>
+          <li>
+            Sensação Térmica: <span>{Math.round(feelsLike)}° C</span>
+          </li>
+          <ul>
+            <li>
+              Descrição: <span>{weatherCap(weatherDescription)}</span>
+            </li>
+          </ul>
+          <ul>
+            <li>
+              Direção do vento: <span>{windDirection(windDeg)}</span>
+            </li>
+            <li>
+              Velocidade do Vento: <span>{windSpeed} m/s</span>
+            </li>
+          </ul>
+          <ul>
+            <li>
+              Nascer do Sol:{' '}
+              <span>
+                <SimpleDateTime {...simpleDateOptions}>
+                  {sunrise}
+                </SimpleDateTime>
+              </span>
+            </li>
+            <li>
+              Pôr do Sol:{' '}
+              <span>
+                <SimpleDateTime {...simpleDateOptions}>{sunset}</SimpleDateTime>
+              </span>
+            </li>
+          </ul>
         </ul>
       </aside>
     </section>
--- src/templates/Home.jsx
+++ src/templates/Home.jsx
@@ -1,6 +1,9 @@
-import { useEffect } from 'react';
+import styles from './Home.module.css';
+
 import { useState } from 'react';
 import Card from '../Components/Card/Card';
+
+import { FcSearch } from 'react-icons/fc';
 
 const Home = () => {
   const [inputCidade, setInputCidade] = useState('');
@@ -9,7 +12,7 @@ const Home = () => {
   const handleClick = () => {
     const fetchCidade = async () => {
       const data = await fetch(
-        `http://api.openweathermap.org/data/2.5/weather?q=${inputCidade},br&APPID=f9d31ae794e0381cd7779d34934d61f8&units=metric`,
+        `http://api.openweathermap.org/data/2.5/weather?q=${inputCidade},br&APPID=f9d31ae794e0381cd7779d34934d61f8&units=metric&lang=pt_br`,
       );
 
       const json = await data.json();
@@ -31,16 +34,19 @@ const Home = () => {
   // temp_min: 24.98;
 
   return (
-    <main>
-      <label>
-        Cidade:
-        <input
-          type="text"
-          value={inputCidade}
-          onChange={({ target }) => setInputCidade(target.value)}
-        />
-      </label>
-      <button onClick={handleClick}>Pesquisar</button>
+    <main className={styles.main}>
+      <div className={styles.input}>
+        <label>
+          Cidade: <FcSearch />
+          <input
+            type="text"
+            value={inputCidade}
+            onChange={({ target }) => setInputCidade(target.value)}
+          />
+        </label>
+        <button onClick={handleClick}>Pesquisar</button>
+      </div>
+
       {resultadoCidade === null ? (
         ''
       ) : (
@@ -48,6 +54,11 @@ const Home = () => {
           temp={resultadoCidade.main.temp}
           humidity={resultadoCidade.main.humidity}
           feelsLike={resultadoCidade.main.feels_like}
+          weatherDescription={resultadoCidade.weather[0].description}
+          windDeg={resultadoCidade.wind.deg}
+          windSpeed={resultadoCidade.wind.speed}
+          sunrise={resultadoCidade.sys.sunrise}
+          sunset={resultadoCidade.sys.sunset}
         />
       )}
     </main>
